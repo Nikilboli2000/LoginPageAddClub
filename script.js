cdbdff0d958984d423c14c30a2700d1c1831f3db
Nikilboli2000 <164467336+Nikilboli2000@users.noreply.github.com>
2024-09-21 10:55:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function loginAlert() {
+    alert("Login successful!");
+    return true; // Allow form submission
+}
+
+function addEventAlert() {
+    alert("Event added successfully!");
+    return true; // Allow form submission
+}
